8c66ef7c70c3a6036de7ca64ec5b93665c9f3e07
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2024-12-09 22:07:00 +0300

    Update server.js

--- Scripts/server.js
+++ Scripts/server.js
@@ -4,29 +4,24 @@ const Task = require('./db');
 const app = express();
 app.use(express.json());
 
-// Обслуживание главной страницы
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'Main.html'));
 });
 
-// Обслуживание статических файлов
 app.use('/Scripts', express.static(path.join(__dirname, '.')));
 app.use('/Styles', express.static(path.join(__dirname, '..', 'Styles')));
 
-// Маршрут для получения задач
 app.get('/tasks', async (req, res) => {
     const tasks = await Task.findAll();
     res.json(tasks);
 });
 
-// Маршрут для добавления задачи
 app.post('/tasks', async (req, res) => {
     const { text } = req.body;
     const task = await Task.create({ text });
     res.json(task);
 });
 
-// Маршрут для удаления всех задач
 app.delete('/tasks', async (req, res) => {
     await Task.destroy({ where: {} });
     res.sendStatus(204);
